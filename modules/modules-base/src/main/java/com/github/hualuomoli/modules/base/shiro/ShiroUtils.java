e280f159b054bdb462dfa190612b028b54d7f32b
hualuomoli <liubaoquan0831@163.com>
2016-01-26 17:23:49 +0800

    add modules and sample

---
+++ modules/modules-base/src/main/java/com/github/hualuomoli/modules/base/shiro/ShiroUtils.java
@@ -0,0 +1,56 @@
+package com.github.hualuomoli.modules.base.shiro;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.subject.Subject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * shiro tool
+ * @author liubaoquan
+ *
+ */
+public class ShiroUtils {
+
+	private static final Logger logger = LoggerFactory.getLogger(ShiroUtils.class);
+
+	private ShiroUtils() {
+	}
+
+	public static void login(String userName, String password) {
+		Subject subject = getSubject();
+		UsernamePasswordToken token = new UsernamePasswordToken();
+		token.setUsername(userName);
+		subject.login(token);
+	}
+
+	public static Subject getSubject() {
+		return SecurityUtils.getSubject();
+	}
+
+	public static Object getPrincipal() {
+		Subject subject = getSubject();
+		return subject.getPrincipal();
+	}
+
+	public static Session getSession() {
+		Subject subject = getSubject();
+		return subject.getSession();
+	}
+
+	public static void put(Object key, Object value) {
+		Session session = getSession();
+		session.setAttribute(key, value);
+		if (logger.isDebugEnabled()) {
+			logger.debug("session timeout default {} s", session.getTimeout() / 1000);
+		}
+	}
+
+	public static Object get(Object key) {
+		Session session = getSession();
+		return session.getAttribute(key);
+	}
+
+}
---
+++ sample/modules-base/src/main/java/com/github/hualuomoli/modules/base/shiro/DefaultUserPermission.java
@@ -0,0 +1,34 @@
+package com.github.hualuomoli.modules.base.shiro;
+
+import java.util.Collection;
+import java.util.Set;
+
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+
+/**
+ * implements demo
+ * @author liubaoquan
+ *
+ */
+public class DefaultUserPermission implements UserPermission {
+
+	@Override
+	public Set<String> getRoles(String userName) {
+		return Sets.newHashSet("admin");
+	}
+
+	@Override
+	public Collection<String> getPermissions(String userName) {
+		return Lists.newArrayList("index");
+	}
+
+	@Override
+	public void extend(SimpleAuthorizationInfo info, String userName) {
+		// add demo permission
+		info.addStringPermission("demo");
+	}
+
+}
