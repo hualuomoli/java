a534a8f62d024a121be88e82ecf03153f5a58bc7
hualuomoli <liubaoquan0831@163.com>
2016-04-10 17:51:47 +0800

    move folder

---
+++ plugins/jpush/src/test/java/com/github/hualuomoli/plugins/jpush/JPushTest.java
@@ -0,0 +1,14 @@
+package com.github.hualuomoli.plugins.jpush;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JPushTest {
+
+	@Test
+	public void testPushNotification() {
+		fail("Not yet implemented");
+	}
+
+}
