f567f6970893cbbf48f90d88ea4a23d399d826cf
hualuomoli <liubaoquan0831@163.com>
2016-04-13 14:58:32 +0800

    update jpush

--- plugins/jpush/src/main/java/com/github/hualuomoli/plugins/jpush/android/JPushAndroid.java
+++ plugins/jpush/src/main/java/com/github/hualuomoli/plugins/jpush/android/JPushAndroid.java
@@ -2,6 +2,7 @@ package com.github.hualuomoli.plugins.jpush.android;
 
 import java.util.Map;
 
+import com.github.hualuomoli.plugins.jpush.JPush;
 import com.github.hualuomoli.plugins.jpush.JPushAbstract;
 import com.github.hualuomoli.plugins.jpush.exception.JPushException;
 
@@ -12,9 +13,17 @@ import cn.jpush.api.push.model.notification.Notification;
 
 public class JPushAndroid extends JPushAbstract {
 
+	private static JPush jpush;
+
+	public static JPush getInstance() {
+		if (jpush == null) {
+			jpush = new JPushAndroid();
+		}
+		return jpush;
+	}
+
 	@Override
-	public PushPayload builderNotification(String alert, String title, Map<String, String> extras)
-			throws JPushException {
+	public PushPayload builderNotification(String alert, String title, Map<String, String> extras) throws JPushException {
 		return PushPayload.newBuilder() //
 				.setPlatform(Platform.all())//
 				.setAudience(Audience.all()) //
--- plugins/jpush/src/main/java/com/github/hualuomoli/plugins/jpush/ios/JPushIOS.java
+++ plugins/jpush/src/main/java/com/github/hualuomoli/plugins/jpush/ios/JPushIOS.java
@@ -2,6 +2,7 @@ package com.github.hualuomoli.plugins.jpush.ios;
 
 import java.util.Map;
 
+import com.github.hualuomoli.plugins.jpush.JPush;
 import com.github.hualuomoli.plugins.jpush.JPushAbstract;
 import com.github.hualuomoli.plugins.jpush.exception.JPushException;
 
@@ -12,9 +13,17 @@ import cn.jpush.api.push.model.notification.Notification;
 
 public class JPushIOS extends JPushAbstract {
 
+	private static JPush jpush;
+
+	public static JPush getInstance() {
+		if (jpush == null) {
+			jpush = new JPushIOS();
+		}
+		return jpush;
+	}
+
 	@Override
-	public PushPayload builderNotification(String alert, String title, Map<String, String> extras)
-			throws JPushException {
+	public PushPayload builderNotification(String alert, String title, Map<String, String> extras) throws JPushException {
 		return PushPayload.newBuilder() //
 				.setPlatform(Platform.all())//
 				.setAudience(Audience.all()) //
