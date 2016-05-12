f7e5aa70c05761a0280ecd681070c2f443c14fd6
hualuomoli <liubaoquan0831@163.com>
2016-05-12 15:56:07 +0800

    update to publish mode

--- tool/raml/front/app.js
+++ tool/raml/front/app.js
@@ -34,10 +34,11 @@ app.use(bodyParser.urlencoded({
 app.use(cookieParser());
 
 // static
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/bower_components', express.static(path.join(__dirname, 'bower_components')));
-app.use(express.static(path.join(__dirname, 'src')));
+// development
+// app.use('/bower_components', express.static(path.join(__dirname, 'bower_components')));
+// app.use(express.static(path.join(__dirname, 'src')));
 
 
 // route
