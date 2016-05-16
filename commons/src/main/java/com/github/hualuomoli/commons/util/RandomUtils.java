95a91b3e22856317d1494a7e77d48f81b7294f16
hualuomoli <liubaoquan0831@163.com>
2016-05-16 17:29:23 +0800

    add method

--- commons/src/main/java/com/github/hualuomoli/commons/util/RandomUtils.java
+++ commons/src/main/java/com/github/hualuomoli/commons/util/RandomUtils.java
@@ -9,12 +9,32 @@ import java.util.UUID;
  */
 public class RandomUtils {
 
+	private static final String[] numbers = new String[] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
 	/**
 	 * 获取ID(32位)
 	 * @return 32随机ID
 	 */
 	public static String getString() {
 		return UUID.randomUUID().toString().replaceAll("[-]", "");
+	}
+
+	/**
+	 * 获取数值
+	 * @param length 长度
+	 * @return 随机数值
+	 */
+	public static String getNumber(int length) {
+		StringBuilder buffer = new StringBuilder();
+		for (int i = 0; i < length; i++) {
+			buffer.append(numbers[getIndex(numbers.length)]);
+		}
+		return buffer.toString();
+	}
+
+	// 获取位置
+	private static final int getIndex(int size) {
+		return (int) (Math.random() * size);
 	}
 
 }
