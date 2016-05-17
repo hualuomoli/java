0ab2afa35fc66885327d2bef13b2876e80f49999
hualuomoli <liubaoquan0831@163.com>
2016-05-17 15:18:24 +0800

    move to https://github.com/hualuomoli/raml.git

--- tool/raml/front/config/config.js
+++
@@ -1,7 +0,0 @@
-//
-module.exports = {
-  port: 3000,
-  env: 'development', // development,publish
-  ios: 'windows', // windows,linux
-  scriptSuffix: 'bat' // 后缀
-}
--- tool/raml/front/script/create.bat
+++
@@ -1,3 +0,0 @@
-@echo OFF
-
-raml2html ../files/%1.raml > ../files/%1.html
