16264f84abbe338e772df45919557366f08061f3
hualuomoli <liubaoquan0831@163.com>
2016-08-04 18:11:25 +0800

    add code exception deal

--- core/src/main/java/com/github/hualuomoli/mvc/config/ExceptionConfig.java
+++ core/src/main/java/com/github/hualuomoli/mvc/config/ExceptionConfig.java
@@ -18,6 +18,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.github.hualuomoli.commons.util.JsonUtils;
 import com.github.hualuomoli.exception.AuthException;
+import com.github.hualuomoli.exception.CodeException;
+import com.github.hualuomoli.exception.CodeException.CodeError;
 import com.github.hualuomoli.exception.InvalidEntityException;
 import com.google.common.collect.Maps;
 
@@ -33,32 +35,41 @@ public class ExceptionConfig {
 
 	private static final Logger logger = LoggerFactory.getLogger(ExceptionConfig.class);
 
+	private static final Integer INVALID_ENTITY_EXCEPTION = 9999; // 参数不合法
+
+	// 业务错误
+	@ExceptionHandler(value = CodeException.class)
+	@ResponseStatus(HttpStatus.OK)
+	public void invalidEntity(HttpServletResponse response, CodeException e) {
+		this.flushJson(response, e.getCodeError());
+	}
+
 	// 实体类不合法
 	@ExceptionHandler(value = InvalidEntityException.class)
 	@ResponseStatus(HttpStatus.OK)
 	public void invalidEntity(HttpServletResponse response, InvalidEntityException e) {
-		this.flushJson(response, Code.InvalidEntityException, StringUtils.join(e.getErrors(), ","));
+		this.flushJson(response, INVALID_ENTITY_EXCEPTION, StringUtils.join(e.getErrors(), ","));
 	}
 
 	// 没有权限
 	@ExceptionHandler(value = AuthException.class)
 	@ResponseStatus(HttpStatus.UNAUTHORIZED)
 	public void unAuthorized(HttpServletResponse response, AuthException e) {
-		this.flushJson(response, Code.AuthException, e.getMessage());
+		this.flushJson(response, HttpStatus.UNAUTHORIZED.value(), e.getMessage());
 	}
 
 	// 请求方法不允许
 	@ExceptionHandler(value = HttpRequestMethodNotSupportedException.class)
 	@ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
 	public void methodNotAllowed(HttpServletResponse response, HttpRequestMethodNotSupportedException e) {
-		this.flushJson(response, Code.METHOD_NOT_ALLOWED, e.getMessage());
+		this.flushJson(response, HttpStatus.METHOD_NOT_ALLOWED.value(), e.getMessage());
 	}
 
 	// 请求协议不允许
 	@ExceptionHandler(value = HttpMediaTypeNotSupportedException.class)
 	@ResponseStatus(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
 	public void unsupportedMediaType(HttpServletResponse response, HttpMediaTypeNotSupportedException e) {
-		this.flushJson(response, Code.UNSUPPORTED_MEDIA_TYPE, e.getMessage());
+		this.flushJson(response, HttpStatus.METHOD_NOT_ALLOWED.value(), e.getMessage());
 	}
 
 	// 运行时异常
@@ -70,10 +81,10 @@ public class ExceptionConfig {
 			this.flushHtml(response, e);
 		} else if (logger.isWarnEnabled()) {
 			logger.warn("{}", e);
-			this.flushJson(response, Code.Exception, e.getMessage());
+			this.flushJson(response, HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
 		} else {
 			logger.error("{}", e);
-			this.flushJson(response, Code.Exception, "系统错误");
+			this.flushJson(response, HttpStatus.INTERNAL_SERVER_ERROR.value(), "系统错误");
 		}
 
 	}
@@ -102,7 +113,7 @@ public class ExceptionConfig {
 	}
 
 	// 输出JSON
-	private void flushJson(HttpServletResponse response, Code code, String message) {
+	private void flushJson(HttpServletResponse response, CodeError codeError) {
 
 		// 设置响应头
 		response.setCharacterEncoding("UTF-8");
@@ -110,8 +121,8 @@ public class ExceptionConfig {
 
 		// 设置数据
 		Map<String, Object> map = Maps.newHashMap();
-		map.put("code", code.getValue());
-		map.put("msg", message);
+		map.put("code", codeError.getCode());
+		map.put("msg", codeError.getMessage());
 		String data = JsonUtils.toJson(map);
 		// 输出
 		try {
@@ -120,24 +131,22 @@ public class ExceptionConfig {
 		}
 	}
 
-	// 响应编码
-	enum Code {
+	// 输出JSON
+	private void flushJson(HttpServletResponse response, Integer code, String message) {
 
-		AuthException("401"), // 没有权限
-		NOT_FOUND("404"), // 没找到
-		METHOD_NOT_ALLOWED("405"), // 方法不允许
-		UNSUPPORTED_MEDIA_TYPE("415"), // 协议不允许
-		Exception("500"), // 系统错误
-		InvalidEntityException("9999"); // 参数不合法
+		// 设置响应头
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("application/json;charset=utf-8");
 
-		private String value;
-
-		private Code(String value) {
-			this.value = value;
-		}
-
-		public String getValue() {
-			return value;
+		// 设置数据
+		Map<String, Object> map = Maps.newHashMap();
+		map.put("code", code);
+		map.put("msg", message);
+		String data = JsonUtils.toJson(map);
+		// 输出
+		try {
+			response.getOutputStream().write(data.getBytes());
+		} catch (IOException e1) {
 		}
 	}
 
