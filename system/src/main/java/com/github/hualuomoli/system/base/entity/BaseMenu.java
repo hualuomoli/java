e0af61ad0fb9e021a5a1c6c1616e172c1b21ee3a
hualuomoli <liubaoquan0831@163.com>
2016-08-10 09:41:16 +0800

    update

---
+++ system/src/main/java/com/github/hualuomoli/system/base/entity/BaseMenu.java
@@ -0,0 +1,32 @@
+package com.github.hualuomoli.system.base.entity;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class BaseMenu extends com.github.hualuomoli.system.entity.Menu
+ implements com.github.hualuomoli.base.BasePersistent, com.github.hualuomoli.base.CommonPersistent
+ {
+
+	/**  - 查询数组 */
+	private java.lang.String[] menuCodeArray;
+	
+	public BaseMenu(){
+	}
+	
+	public BaseMenu(
+		java.lang.String menuCode
+	){
+		this.setMenuCode(menuCode);
+	}
+	
+	public java.lang.String[] getMenuCodeArray() {
+		return menuCodeArray;
+	}
+	
+	public void setMenuCodeArray(java.lang.String[] menuCodeArray) {
+		if (menuCodeArray == null || menuCodeArray.length == 0) {
+			return;
+		}
+		this.menuCodeArray = menuCodeArray;
+	}
+
+}
