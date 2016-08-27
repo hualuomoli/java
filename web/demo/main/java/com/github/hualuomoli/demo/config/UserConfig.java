c97ee0aa6c433ee85829ec0e66aa24e55175cc98
hualuomoli <liubaoquan0831@163.com>
2016-08-27 11:11:06 +0800

    update

---
+++ web/demo/main/java/com/github/hualuomoli/demo/config/UserConfig.java
@@ -0,0 +1,55 @@
+package com.github.hualuomoli.demo.config;
+
+import java.util.Date;
+import java.util.HashSet;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.github.hualuomoli.login.service.LoginUserService;
+import com.google.common.collect.Sets;
+
+@Configuration
+public class UserConfig {
+
+	@Bean
+	public LoginUserService loginUserService() {
+		return new LoginUserService() {
+
+			@Override
+			public String getUsername() {
+				return "system";
+			}
+
+			@Override
+			public void login(String key, String username) {
+			}
+
+			@Override
+			public void refresh() {
+			}
+
+			@Override
+			public Date getCurrentDate() {
+				return new Date();
+			}
+
+			@Override
+			public HashSet<String> getUserRoles() {
+				return Sets.newHashSet();
+			}
+
+			@Override
+			public HashSet<String> getUserPermissions() {
+				return Sets.newHashSet();
+			}
+
+			@Override
+			public void logout() {
+
+			}
+
+		};
+	}
+
+}
